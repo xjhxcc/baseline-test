2143ef859152b7bb119252401c4633bde8ab22e4
ARIA XU (ITA-ISDC-ISD-OOCLL/ZHA) <XUAR2@OOCL.COM>
2019-07-08 12:08:02 +0800

    2

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,5 +1,15 @@
-'use strict';
+﻿'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	var result=[];
+	for(var i=0;i<collectionA.length;i++){
+		for(var j=0;j<collectionB.length;j++){
+			if(collectionA[i].equals(collectionB[j])){
+				result.push(collectionA[i]);
+			}
+		}
+	}
+
+
+  return result;
 }
