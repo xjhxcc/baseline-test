21783607a3446d0ca6e1abdb9dbc6ef3116734de
xjhxcc <2351889342@qq.com>
2019-07-08 13:17:00 +0800

    2ok

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -4,8 +4,10 @@ function collectSameElements(collectionA, collectionB) {
 var result=[];
 	for(var i=0;i<collectionA.length;i++){
 		for(var j=0;j<collectionB.length;j++){
-			if(collectionA[i].equals(collectionB[j])){
-				result.push(collectionA[i]);
+			for(var m=0;m<collectionB[j].length;m++){
+				if(collectionA[i]==(collectionB[j][m])){
+					result.push(collectionA[i]);
+				}
 			}
 		}
 	}
