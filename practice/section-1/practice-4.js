6912d40cda9200085eeb797a733cc0e46dd53529
xjhxcc <45287894+xjhxcc@users.noreply.github.com>
2019-07-08 22:28:31 +0800

    4ok

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -2,12 +2,12 @@
 
 function collectSameElements(collectionA, objectB) {
 var result=[];
-	for(var i=0;i<collectionA.length;i++){
-		for(var j=0;j<collectionB.value.length;j++){
-			if(collectionA[i].key.equals(collectionB.value[j])){
-				result.push(collectionA[i].key);
-			}
-		}
-	}
+  for(var i=0;i<collectionA.length;i++){
+    for(var j=0;j<objectB.value.length;j++){
+      if(collectionA[i].key==(objectB.value)[j]){
+        result.push(collectionA[i].key);
+      }          
+    }
+  }
   return result;
 }
