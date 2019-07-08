d49508d2ace5319be39268eb05ce294741f339c8
xjhxcc <45287894+xjhxcc@users.noreply.github.com>
2019-07-08 22:27:33 +0800

    3ok

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,13 +1,13 @@
 ﻿'use strict';
 
 function collectSameElements(collectionA, objectB) {
-	 var result=[];
-	for(var i=0;i<collectionA.length;i++){
-		for(var j=0;j<collectionB.value.length;j++){
-			if(collectionA[i].equals(collectionB.value[j])){
-				result.push(collectionA[i]);
-			}
-		}
-	}
+	var result=[];
+  for(var i=0;i<collectionA.length;i++){
+    for(var j=0;j<objectB.value.length;j++){
+      if(collectionA[i]==(objectB.value)[j]){
+        result.push(collectionA[i]);
+      }          
+    }
+  }
   return result;
 }
